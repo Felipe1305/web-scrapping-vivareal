6871fb447ba5e9fee9741de783fb12ab81211595
inteligencia-info4 <inteligencia@info4.com.br>
2022-05-17 11:54:10 -0300

    Encontrando todos os links de anúncios da página

--- main.py
+++ main.py
@@ -6,21 +6,38 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
+# Busca das cidades desejadas
+#cidades = ['Teresópolis, RJ','Petrópolis - RJ', 'Nova Friburgo']
+cidades = ['Teresópolis, RJ']
 browser = webdriver.Chrome(executable_path="./drivers/chromedriver")
-browser.get('https://www.vivareal.com.br/')
-select = browser.find_element_by_xpath(
-    '//*[@id="js-site-main"]/section[1]/div/div/form[1]/div[1]/div/div/div[2]/select')
-select.send_keys(' Mostrar Todos ', Keys.ENTER)
+for value in cidades:
+    browser.get('https://www.vivareal.com.br/')
 
-time.sleep(2)
-element = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.ID, "filter-location-search-input"))
-).send_keys('Teresópolis, RJ')
-time.sleep(2)
-element = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.ID, "filter-location-search-input"))
-).send_keys(Keys.RETURN)
+    select = browser.find_element_by_xpath(
+        '//*[@id="js-site-main"]/section[1]/div/div/form[1]/div[1]/div/div/div[2]/select')
+    select.send_keys(' Mostrar Todos ', Keys.ENTER)
+
+    time.sleep(2)
+    element = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.ID, "filter-location-search-input"))
+    ).send_keys(value)
+    time.sleep(2)
+    element = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.ID, "filter-location-search-input"))
+    ).send_keys(Keys.RETURN)
+
+# Extraindo todos os links de imóveis da cidade
+
+
+# Encontrando o conteiner com os anúncios
+elem = browser.find_element_by_xpath(
+    '//*[@id="js-site-main"]/div[2]/div[1]/section/div[2]/div[1]')
+elements = elem.find_elements_by_tag_name('article')
+
+# Para cada card retirar o link do anúncio
+for ele in elements:
+    print(ele.find_element_by_tag_name('a').get_attribute('href'))
+
 
 time.sleep(8)
 browser.quit()
